39ebc243ef7ff247c0a56e3e64f01dfd113e3b8a
sayem <mirajussalehin@gmail.com>
2020-10-10 22:44:13 +0600

    Update passwordGenerator.py

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,9 +1,17 @@
 import itertools
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-p',action='store',dest='possibleCombo',help="Possible combination",type=str,default=3)
-parser.add_argument('-c',action='store',dest='combinationType',help="Combination type",type=str)
+
+_doc = """(REQUIRED) Possible options for password combination type:
+1 :  Combination Alphnumeric .
+2 :  Combination Character only .
+3 :  Combination Special Character Only .
+4 :  Combination Special Character and number only .
+5 :  Combination Alphanumeric Special Character .
+"""
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-p',action='store',dest='possibleCombo',help="Number of characters in password (DEFAULT = 3)",type=str,default=3)
+parser.add_argument('-c',action='store',dest='combinationType',help=_doc,type=str,required=True)
 
 
 
@@ -37,7 +45,7 @@ def generatePass(l):
 count = 0
 for x in generatePass(getCarecter):
     f = open("passwordList.txt", "a")
-    f.write(''.join(x)+"\n")
+    f.write(''.join(x)+".")
     count = count + 1
     print(str(count) + " Possible Combination=> "+ ''.join(x))
 
