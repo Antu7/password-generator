fe1ec67e09124c5245bcd914492e9ea0e3db69dc
Tanvir Hossain Antu <textmeantu@gmail.com>
2023-01-10 16:19:48 +0600

    OOP coding style updated

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,43 +1,42 @@
 import itertools
 
-possibleCombo = input("How Many Password Combination You Want to Create? Exp(3): ")
+class PasswordGenerator:
+    def __init__(self, possible_combination: int, combination_type: int):
+        self.possible_combination = possible_combination
+        self.combination_type = combination_type
+        self.special = '!"#$%&\'()*+,-. /:;?@[]^_`{|}~'
+        self.numeric = '0123456789'
+        self.alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        self.get_carecter = ""
+        
+    def generate_get_carecter(self):
+        if self.combination_type == 1:
+            self.get_carecter = self.numeric + self.alphabet
+        elif self.combination_type == 2:
+            self.get_carecter = self.numeric
+        elif self.combination_type == 3:
+            self.get_carecter = self.alphabet
+        elif self.combination_type == 4:
+            self.get_carecter = self.special
+        elif self.combination_type == 5:
+            self.get_carecter = self.special + self.numeric
+        elif self.combination_type == 6:
+            self.get_carecter = self.special + self.numeric + self.alphabet
+        else:
+            raise ValueError("Invalid combination_type")
 
-combinationType = input("1) Combination Alphanumeric \n"
-"2) Combination Numeric only \n"
-"3) Combination Carecter only \n"
-"4) Combination special Carecter only \n"
-"5) Combination special Carecter & number only \n"
-"6) Combination Alphanumeric Special Characters \n"
-"==>  "
-)
+    def generate_password(self):
+        for x in itertools.product(*([self.get_carecter] * self.possible_combination)):
+            yield ''.join(x)
 
-special = '!"#$%&\'()*+,-. /:;?@[]^_`{|}~'
-numeric = '0123456789'
-carecter = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    def write_to_file(self):
+        with open("password_list.txt", "w") as file:
+            for i, password in enumerate(self.generate_password()):
+                file.write(password + "\n")
+                print(f"{i+1} possible combination: {password}")
 
-if combinationType == '1':
-    getCarecter = numeric + carecter
-elif combinationType == '2':
-    getCarecter = numeric
-elif combinationType == '3':
-    getCarecter = carecter
-elif combinationType == '4':
-    getCarecter = special
-elif combinationType == '5':
-    getCarecter = special + numeric
-elif combinationType == '6':
-    getCarecter = special + numeric + carecter
-else:
-    exit("Bad Input")
-
-def generatePass(l):
-    yield from itertools.product(*([l] * int(possibleCombo))) 
-
-count = 0
-for x in generatePass(getCarecter):
-    f = open("passwordList.txt", "a")
-    f.write(''.join(x)+"\n")
-    count = count + 1
-    print(str(count) + " Possible Combination=> "+ ''.join(x))
-
-f.close()
+possible_combination = int(input("How many password combinations do you want to create? Exp(3): "))
+combination_type = int(input("Enter combination type (1-6): "))
+generator = PasswordGenerator(possible_combination, combination_type)
+generator.generate_get_carecter()
+generator.write_to_file()
